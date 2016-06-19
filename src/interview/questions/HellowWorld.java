a4cad512aa5e289035031d5243914dcce22666d4
reddyprasad481 <reddyprasad481@gmail.com>
2016-06-19 23:32:44 +0530

    new classes added

---
+++ src/interview/questions/HellowWorld.java
@@ -0,0 +1,14 @@
+package interview.questions;
+
+public class HellowWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hellow World");
+	}
+
+}
